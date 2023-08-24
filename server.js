f451ce398207001fed744ab8e983336fbacdcbb6
Hafeez <90968109+hafeezhmha@users.noreply.github.com>
2023-08-24 13:22:22 +0530

    Added yam1 file for render

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
